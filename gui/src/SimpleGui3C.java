c0e9c1e24789ca3062185fc3e915b49ccbea1a8a
may <wxm112@me.com>
2015-10-14 10:53:26 +1100

    added all

--- gui/src/SimpleGui3C.java
+++ gui/src/SimpleGui3C.java
@@ -1,6 +1,13 @@
+import javax.swing.*;
+
 /**
  * Created by mayw on 14/10/2015.
  */
 public class SimpleGui3C {
     JFrame frame;
+
+    public static void main (String[] args) {
+        SimpleGui3C gui = new SimpleGui3C();
+        gui.go();
+    }
 }
